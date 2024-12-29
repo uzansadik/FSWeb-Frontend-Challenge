d9bb0159340c06b66e88f81c7d73ddfb42958b8c
uzansadik <uzansadik@gmail.com>
2024-12-30 01:23:52 +0300

    first-commit

--- uzanWeb/src/App.jsx
+++ uzanWeb/src/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from './contexts/theme-provider';
 import LanguageContextProvider from './contexts/LanguageContext';
 import Header from './components/section/Header';
 import Navigation from './components/section/Navigation';
+import Hero from './components/section/Hero';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <ThemeProvider defaultTheme='light' storageKey='theme'>
         <Header></Header>
         <Navigation></Navigation>
+        <Hero></Hero>
       </ThemeProvider>
     </LanguageContextProvider>
   );
---
+++ uzanWeb/src/components/section/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section className='flex flex-col mt-10'>
+      <div className='hero-lef'></div>
+      <img src='' alt='' />
+    </section>
+  );
+}
+
+export default Hero;
